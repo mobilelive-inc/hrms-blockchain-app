cf9c74eb399daf5172421838e85e4763509f7a00
Huzaifa Shahzad Khan <huzaifa.shahzad@mobilelive.ca>
2023-06-13 18:37:38 +0500

    UI improvements in GitModal and getFiles api working properly

--- src/pages/Employee/GitModal.js
+++ src/pages/Employee/GitModal.js
@@ -18,25 +18,29 @@ class ModalComponentGit extends React.Component {
 
           <div style={{ margin: "30px", padding: "5px" }}>
             <div className="inner-div">
-              {this.props?.commits &&
+              {this.props?.commits.length!==0 ?(
                 this.props?.commits?.map((group) => (
-                  <div className="inner-div" key={group.date}>
+                  <div className="inner-div" key={group?.date}>
                     <li className="commit-date-style">Commits on {moment(group?.date).format("DD-MM-YYYY")}</li>
                     <br/>
+                    
                     {group.commits &&
                       group.commits.map((commit) => (
-                        <div key={commit.sha}>
-                          <div style={{ marginBottom: "10px" }}>
-                          <h2>{commit?.commit?.message}</h2>
-                            
-                          </div>
+                        <div className="commit-info" key={commit?.sha}>
+                          <a href={commit?.html_url} style={{ marginBottom: "10px" }}>
+                          <h2 className="message-style">{commit?.commit?.message}</h2>
+                          </a>
+                          <div className="author-info">
+                          <Icon className="user-icon" name="user circle"/>
                           <h4>{commit?.commit?.author?.name}</h4>
-
+                          </div>
                           <hr className="divider" />
                         </div>
                       ))}
+                      
                   </div>
-                ))}
+                ))):<h4>No commits to display for this Repo!</h4>
+              }
             </div>
           </div>
         </div>
